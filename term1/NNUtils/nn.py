41113b82640c9594e98bfe24f2f00914b7536691
Abhas <abhasforyou@gmail.com>
2017-02-12 23:22:47 -0800

    Add sigmoid node and tests for it

--- term1/NNUtils/nn.py
+++ term1/NNUtils/nn.py
@@ -54,6 +54,16 @@ class Linear(Node):
         self.value = np.dot(features.value, weights.value) + bias.value
 
 
+class Sigmoid(Node):
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        return 1./(1. + np.exp(-x))
+
+    def forward(self):
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
+
 #### Utility methods #####
 
 def topological_sort(feed_dict):
--- term1/NNUtils/test_nn.py
+++ term1/NNUtils/test_nn.py
@@ -12,14 +12,33 @@ assert output == 10, "Output %d is different than expected" % output
 print("Add node test passed")
 
 
-## Linear Node
+## Linear Node with 1-d inputs
 features, weights, bias = Input(), Input(), Input()
 linear = Linear(features, weights, bias)
 feed_dict = {features: np.array((1, 2, 3)), weights: np.array((2,3,4)), bias: 3}
 output = forward_pass(linear, topological_sort(feed_dict))
 assert output == 23, "Output %d is different than expected" % output
-print("Linear node test passed")
+print("Linear node 1-d inputs test passed")
 
+## Linear Node with 2-d inputs
+features, weights, bias = Input(), Input(), Input()
+linear = Linear(features, weights, bias)
+feed_dict = {features: np.array([[-1., -2.], [-1, -2]]), weights: np.array([[2., -3], [2., -3]]),
+             bias: np.array([-3., -5])}
+output = forward_pass(linear, topological_sort(feed_dict))
+assert np.array_equal(output, np.array([[-9., 4.], [-9., 4.]])) == True, "Output %d is different than expected" % output
+print("Linear node 2-d inputs test passed")
+
+## Linear + Sigmoid with 2-d inputs
+features, weights, bias = Input(), Input(), Input()
+linear = Linear(features, weights, bias)
+sigmoid = Sigmoid(linear)
+feed_dict = {features: np.array([[-1., -2.], [-1, -2]]), weights: np.array([[2., -3], [2., -3]]),
+             bias: np.array([-3., -5])}
+output = forward_pass(sigmoid, topological_sort(feed_dict))
+expected_output = np.array([[1.23394576e-04, 9.82013790e-01], [1.23394576e-04, 9.82013790e-01]])
+np.testing.assert_array_almost_equal(output, expected_output) == True
+print("Linear + Sigmoid node 2-d inputs test passed")
 
 
 
