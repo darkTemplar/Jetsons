81777d3ac720be0e48d6a3540fcbf18ce3e1ca54
Abhas <abhasforyou@gmail.com>
2017-03-04 11:01:07 -0800

    Add final code for traffic sign classification

--- term1/deepLearningTutorial/traffic-signs/src/Traffic_Sign_Classifier.py
+++ term1/deepLearningTutorial/traffic-signs/src/Traffic_Sign_Classifier.py
@@ -6,15 +6,32 @@
 # ## Deep Learning
 # 
 # ## Project: Build a Traffic Sign Recognition Classifier
+# 
+# In this notebook, a template is provided for you to implement your functionality in stages, which is required to successfully complete this project. If additional code is required that cannot be included in the notebook, be sure that the Python code is successfully imported and included in your submission if necessary. 
+# 
+# > **Note**: Once you have completed all of the code implementations, you need to finalize your work by exporting the iPython Notebook as an HTML document. Before exporting the notebook to html, all of the code cells need to have been run so that reviewers can see the final implementation and output. You can then export the notebook by using the menu above and navigating to  \n",
+#     "**File -> Download as -> HTML (.html)**. Include the finished document along with this notebook as your submission. 
+# 
+# In addition to implementing code, there is a writeup to complete. The writeup should be completed in a separate file, which can be either a markdown file or a pdf document. There is a [write up template](https://github.com/udacity/CarND-Traffic-Sign-Classifier-Project/blob/master/writeup_template.md) that can be used to guide the writing process. Completing the code template and writeup template will cover all of the [rubric points](https://review.udacity.com/#!/rubrics/481/view) for this project.
+# 
+# The [rubric](https://review.udacity.com/#!/rubrics/481/view) contains "Stand Out Suggestions" for enhancing the project beyond the minimum requirements. The stand out suggestions are optional. If you decide to pursue the "stand out suggestions", you can include the code in this Ipython notebook and also discuss the results in the writeup file.
+# 
+# 
+# >**Note:** Code and Markdown cells can be executed using the **Shift + Enter** keyboard shortcut. In addition, Markdown cells can be edited by typically double-clicking the cell to enter edit mode.
 
+# ---
 # ## Step 0: Load The Data
+
+# In[13]:
 
 # Load pickled data
 import pickle
 
-training_file = '../data/train.p'
-validation_file= '../data/valid.p'
-testing_file = '../data/test.p'
+# TODO: Fill this in based on where you saved the training and testing data
+
+training_file = 'train.p'
+validation_file= 'valid.p'
+testing_file = 'test.p'
 
 with open(training_file, mode='rb') as f:
     train = pickle.load(f)
@@ -24,6 +41,7 @@ with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
     
 X_train, y_train = train['features'], train['labels']
+original_X_train, original_y_train = X_train[:5000], y_train[:5000]
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
@@ -43,7 +61,7 @@ X_test, y_test = test['features'], test['labels']
 
 # ### Provide a Basic Summary of the Data Set Using Python, Numpy and/or Pandas
 
-# In[ ]:
+# In[14]:
 
 ### Replace each question mark with the appropriate value. 
 ### Use python, pandas or numpy methods rather than hard coding the results
@@ -70,27 +88,22 @@ print("Number of classes =", n_classes)
 # 
 # **NOTE:** It's recommended you start with something simple first. If you wish to do more, come back to it after you've completed the rest of the sections.
 
-# In[16]:
+# In[15]:
 
 ### Data exploration visualization code goes here.
 ### Feel free to use as many code cells as needed.
 import matplotlib.pyplot as plt
-from collections import Counter
-
-train_label_frequencies = dict(Counter(y_train))
-plt.bar(list(train_label_frequencies.keys()), list(train_label_frequencies.values()), 1, color='r')
-
-
 import numpy as np
 import cv2
+# Visualizations will be shown in the notebook.
+get_ipython().magic('matplotlib inline')
 
-index = np.random.randint(0, len(X_train))
-image = X_train[index]
-image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
-plt.figure(figsize=(1,1))
-plt.imshow(image, cmap="gray")
-print(y_train[index])
+# plot frequencies of various classes (43 in total) in the training dataset
+from collections import Counter
+train_label_frequencies = dict(Counter(y_train))
+plt.bar(list(train_label_frequencies.keys()), list(train_label_frequencies.values()), 1., color='r')
+# print number of labels which have less than 500 examples
+print("Number of label classes with less than 500 examples in training data ",sum([1 for k,v in train_label_frequencies.items() if v < 500]))
 
 
 # ----
@@ -114,18 +127,164 @@ print(y_train[index])
 
 # Use the code cell (or multiple code cells, if necessary) to implement the first step of your project.
 
-# In[4]:
+# In[17]:
 
 ### Preprocess the data here. Preprocessing steps could include normalization, converting to grayscale, etc.
 ### Feel free to use as many code cells as needed.
 
+import cv2
+
+def augmentImageData(images, labels, passes=4):
+    global train_label_frequencies
+    num_examples = len(labels)
+    new_images, new_labels = [], []
+    translate, scale, rotate = False, False, False
+    for i in range(passes):
+        for j in range(num_examples):
+            image, label = images[j], labels[j]
+            rows,cols = image.shape[:2]
+            # if none of the transformations apply AND label is not under represented class, move on to next image
+            # if label is part of under represented i.e. less than 500 examples, we force augmentation for that image
+            under_represented = train_label_frequencies.get(label, 0) < 500
+            if under_represented:
+                translate, scale, rotate = True, True, True
+            else:
+                # we do a coin flip to determine which transformations to apply
+                translate, scale, rotate = np.random.uniform() > 0.5, np.random.uniform() > 0.5, np.random.uniform() > 0.5
+            
+            # if none of the transformations apply, move on to next image
+            if not translate and not scale and not rotate:
+                continue
+            if translate:
+                shift_x, shift_y = np.random.randint(-3,3), np.random.randint(-3,3)
+                # create transformation matrix
+                M = np.float32([[1,0,shift_x],[0,1,shift_y]])
+                image = cv2.warpAffine(image,M,(cols,rows))
+                
+            if scale:
+                scale_x, scale_y = np.random.uniform(1., 1.3), np.random.uniform(1., 1.3)
+                image = cv2.resize(image, None, fx=scale_x, fy=scale_y, interpolation = cv2.INTER_CUBIC)
+                # TODO: crop image to convert it back to 32 x 32
+                image_height, image_width = image.shape
+                padding_height, padding_width = image_height - 32, image_width - 32
+                if padding_height % 2 == 0:
+                    image = image[padding_height//2:image_height - padding_height//2, padding_width//2:image_width - padding_width//2] if padding_width % 2 == 0 else image[padding_height//2:image_height - padding_height//2, padding_width//2:image_width - (padding_width+1)//2]
+                else:
+                    image = image[padding_height//2:image_height - (padding_height+1)//2, padding_width//2:image_width - padding_width//2] if padding_width % 2 == 0 else image[padding_height//2:image_height - (padding_height+1)//2, padding_width//2:image_width - (padding_width+1)//2]
+                
+                
+            if rotate:
+                rotation_degrees = np.random.randint(-15,15)
+                M = cv2.getRotationMatrix2D((cols/2,rows/2),rotation_degrees,1)
+                image = cv2.warpAffine(image,M,(cols,rows))
+
+            new_images.append(image[:,:,np.newaxis])
+            new_labels.append(label)
+    return np.concatenate([images, np.array(new_images)]), np.concatenate([labels, np.array(new_labels)])
+
+def preProcessImages(images):   
+    # Pre processing Step 1 - Turn images into grayscale
+    num_images = len(images)
+    gray_images = np.zeros((num_images, 32, 32, 1))
+    for i in range(num_images):
+        image = images[i]
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        image = image[:,:,np.newaxis]
+        gray_images[i] = image
+        
+    # Pre processing Step 2 - Center and normalize the images
+    gray_images -= np.mean(gray_images)
+    return gray_images
+
+# preprocess the training images
+X_train = preProcessImages(X_train)
+original_X_train = preProcessImages(original_X_train)
+# preprocess the validation images
+X_valid = preProcessImages(X_valid)
+
+# visualize a randomly chosen training image
+index = np.random.randint(0, len(X_train))
+image = X_train[index].squeeze()
+plt.imshow(image)
+
+# augment training dataset
+print("num of training examples BEFORE augmentation ", len(X_train))
+X_train, y_train = augmentImageData(X_train, y_train)
+print("num of training examples AFTER training ", len(X_train))
+
+# re-visualize the labels to see if we increased examples in each class
+# plot frequencies of various classes (43 in total) in the training dataset
+train_label_frequencies = dict(Counter(y_train))
+plt.bar(list(train_label_frequencies.keys()), list(train_label_frequencies.values()), 1., color='r')
+# print number of labels which have less than 500 examples
+print("Number of label classes with less than 500 examples in augmented training data ",sum([1 for k,v in train_label_frequencies.items() if v < 500]))
+
 
 # ### Model Architecture
 
-# In[ ]:
+# In[8]:
 
 ### Define your architecture here.
 ### Feel free to use as many code cells as needed.
+import tensorflow as tf
+
+EPOCHS = 15
+BATCH_SIZE = 128
+
+
+
+from tensorflow.contrib.layers import flatten
+
+def AbhasNet(x):
+    global n_classes
+    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    mu = 0
+    sigma = 0.1
+    
+    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x38.
+    w1 = tf.Variable(tf.truncated_normal([5,5,1,38], mean=mu, stddev=sigma))
+    b1 = tf.Variable(tf.zeros(38))
+    layer1 = tf.nn.conv2d(x, w1, strides=[1, 1, 1, 1], padding="VALID")
+    layer1 = tf.nn.bias_add(layer1, b1)
+    # Activation.
+    layer1 = tf.nn.relu(layer1)
+    # Pooling. Input = 28x28x38. Output = 14x14x38.
+    layer1 = tf.nn.max_pool(layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    
+    
+
+    # Layer 2: Convolutional. Output = 10x10x64.
+    w2 = tf.Variable(tf.truncated_normal([5,5,38,64], mean=mu, stddev=sigma))
+    b2 = tf.Variable(tf.zeros(64))
+    layer2 = tf.nn.conv2d(layer1, w2, [1, 1, 1, 1], padding="VALID")
+    layer2 = tf.nn.bias_add(layer2, b2)
+    # Activation.
+    layer2 = tf.nn.relu(layer2)
+    # Pooling. Input = 10x10x80. Output = 5x5x64.
+    layer2 = tf.nn.max_pool(layer2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    # Flatten. Input = 5x5x64. Output = 1600.
+    layer2 = flatten(layer2)
+    
+    # Layer 3: Fully Connected. Input = 1600. Output = 200.
+    w3 = tf.Variable(tf.truncated_normal([1600, 200], mean=mu, stddev=sigma))
+    b3 = tf.Variable(tf.zeros(200))
+    layer3 = tf.add(tf.matmul(layer2, w3), b3)
+    # Activation.
+    layer3 = tf.nn.relu(layer3)
+    
+    # Layer 4: Fully Connected. Input = 200. Output = 100.
+    w4 = tf.Variable(tf.truncated_normal([200, 100], mean=mu, stddev=sigma))
+    b4 = tf.Variable(tf.zeros(100))
+    layer4 = tf.add(tf.matmul(layer3, w4), b4)
+    # Activation.
+    layer4 = tf.nn.relu(layer4)
+    
+    # Layer 5: Fully Connected. Input = 100. Output = 43.
+    w5 = tf.Variable(tf.truncated_normal([100, n_classes], mean=mu, stddev=sigma))
+    b5 = tf.Variable(tf.zeros(n_classes))
+    logits = tf.add(tf.matmul(layer4, w5), b5)
+    print("Logits defined for ", n_classes)
+    return logits
 
 
 # ### Train, Validate and Test the Model
@@ -133,13 +292,95 @@ print(y_train[index])
 # A validation set can be used to assess how well the model is performing. A low accuracy on the training and validation
 # sets imply underfitting. A high accuracy on the training set but low accuracy on the validation set implies overfitting.
 
-# In[1]:
+# In[10]:
 
 ### Train your model here.
 ### Calculate and report the accuracy on the training and validation set.
 ### Once a final model architecture is selected, 
 ### the accuracy on the test set should be calculated and reported as well.
 ### Feel free to use as many code cells as needed.
+ 
+# `x` is a placeholder for a batch of input images.
+# `y` is a placeholder for a batch of output labels.
+# `keep_prob` is a placeholder for dropout prob. for the fully connected layers.
+
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+keep_prob3 = tf.placeholder(tf.float32)
+#keep_prob4 = tf.placeholder(tf.float32)
+one_hot_y = tf.one_hot(y, n_classes)
+
+
+# ## Training Pipeline
+
+rate = 0.001
+
+logits = AbhasNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+
+# ## Model Evaluation
+# Evaluate how well the loss and accuracy of the model for a given dataset.
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+# ## Train the Model
+# Run the training data through the training pipeline to train the model.
+# 
+# Before each epoch, shuffle the training set.
+# 
+# After each epoch, measure the loss and accuracy of the validation set.
+# 
+# Save the model after training.
+from sklearn.utils import shuffle
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+        
+        #training_accuracy = evaluate(original_X_train, original_y_train)
+        print("EPOCH {} ...".format(i+1))
+        #print("Training Accuracy = {:.3f}".format(training_accuracy))
+        validation_accuracy = evaluate(X_valid, y_valid)
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+        
+    saver.save(sess, './abhasnet')
+    print("Model saved")
+
+    
+with tf.Session() as sess:
+    # preprocess test set
+    X_test = preProcessImages(X_test)
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+    test_accuracy = evaluate(X_test, y_test)
+    print("Test Accuracy = {:.3f}".format(test_accuracy))
 
 
 # ---
@@ -152,28 +393,61 @@ print(y_train[index])
 
 # ### Load and Output the Images
 
-# In[ ]:
+# In[42]:
 
 ### Load the images and plot them here.
 ### Feel free to use as many code cells as needed.
+import glob, os
+# we add the data type to the web traffic images declaration 
+# so that it will play nice with cv2.cvtColor which is present in the preprocessing function
+web_traffic_images = np.empty((5, 32, 32, 3), np.uint8)
+idx = 0
+for file in glob.glob("*.jpeg"):
+    print("Reading file ", file)
+    plt.imshow(image)
+    plt.show()
+    image = cv2.imread(file, 1)
+    image = cv2.resize(image, (32, 32), 0, 0, interpolation = cv2.INTER_CUBIC)
+    web_traffic_images[idx] = image
+    idx += 1
 
 
 # ### Predict the Sign Type for Each Image
 
-# In[3]:
+# In[40]:
 
 ### Run the predictions here and use the model to output the prediction for each image.
 ### Make sure to pre-process the images with the same pre-processing pipeline used earlier.
 ### Feel free to use as many code cells as needed.
+# ## Evaluate the Model
+# Once you are completely satisfied with your model, evaluate the performance of the model on the test set.
+# 
+# Be sure to only do this once!
+# 
+# If you were to measure the performance of your trained model on the test set, then improve your model, and then measure the performance of your model on the test set again, that would invalidate your test results. You wouldn't get a true measure of how well your model would perform against real data.
+# 
+# You do not need to modify this section.
+
+# run preprocessing on the 5 web traffic images
+web_traffic_images = preProcessImages(web_traffic_images)
+
+# add labels for the web traffic images in the following order:
+# (keep right, no entry, road work, 60 speed limit, bumpy road)
+y_web = np.array([38,17,25,3,24], np.uint8)
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+    web_test_accuracy = evaluate(web_traffic_images, y_web)
+    print("Test Accuracy = {:.3f}".format(web_test_accuracy))
+    softmax = tf.nn.softmax(logits=logits)
+    top_k = sess.run(softmax, feed_dict={x: web_traffic_images, y: y_web})
+    print(sess.run(tf.nn.top_k(tf.constant(top_k), k=3)))
+
+# In[21]:
 
 
 # ### Analyze Performance
 
-# In[4]:
-
-### Calculate the accuracy for these 5 new images. 
-### For example, if the model predicted 1 out of 5 signs correctly, it's 20% accurate on these new images.
-
+# As we can see from results above we get an accuracy of 40%, correctly predicting the keep right and road work signs.
 
 # ### Output Top 5 Softmax Probabilities For Each Image Found on the Web
 
@@ -215,11 +489,18 @@ print(y_train[index])
 # 
 # Looking just at the first row we get `[ 0.34763842,  0.24879643,  0.12789202]`, you can confirm these are the 3 largest probabilities in `a`. You'll also notice `[3, 0, 5]` are the corresponding indices.
 
-# In[6]:
-
-### Print out the top five softmax probabilities for the predictions on the German traffic sign images found on the web. 
-### Feel free to use as many code cells as needed.
-
+# The top five softmax probabilities for the predictions made above (code is in same cell where we calculated accuracy above)
+# 
+# TopKV2(values=array([[  1.00000000e+00,   0.00000000e+00,   0.00000000e+00],
+#        [  9.31570530e-01,   6.56870678e-02,   2.74247653e-03],
+#        [  1.00000000e+00,   0.00000000e+00,   0.00000000e+00],
+#        [  6.96351230e-01,   1.56670287e-01,   1.46977261e-01],
+#        [  8.00344706e-01,   1.99637681e-01,   1.31297438e-05]], dtype=float32), indices=array([[38,  0,  1],
+#        [34,  7, 17],
+#        [25,  0,  1],
+#        [ 0,  1,  8],
+#        [ 9, 23, 38]], dtype=int32))
+# 
 
 # ---
 # 
